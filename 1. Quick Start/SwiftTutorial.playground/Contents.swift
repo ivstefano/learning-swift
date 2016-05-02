1459b5559ff40c5dde9ff0e3de9ae7c3ebc502e1
Ivo <harbachek@gmail.com>
2016-05-02 19:05:23 +0300

    Initial playground test

---
+++ 1. Quick Start/SwiftTutorial.playground/Contents.swift
@@ -0,0 +1,23 @@
+let swiftTeam = 42
+let iOSTeam = 52
+let otherTeams = 33
+var totalTeams = swiftTeam + iOSTeam + otherTeams
+
+totalTeams += 1
+
+let priceInferred = 19.9
+let priceExplicit: Double = 19.9
+
+let onSaleInferred = true
+let onSaleExplicit: Bool = true
+
+let nameInferred = "Whoopie Cushion"
+let nameExplicit: String = "Whoopie Cushion"
+
+if onSaleInferred {
+    print("\(nameInferred) on sale for  \(priceInferred)")
+} else {
+    print("\(nameInferred) on sale for  \(priceInferred)")
+
+}
+
